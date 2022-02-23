ce94fbe82d22ae6ff5aab9736804eceefc2b5014
rorekk <rorekk@gmail.com>
2022-02-23 02:06:29 -0800

    begin curation team

---
+++ src/routes/curation-team.svelte
@@ -0,0 +1,36 @@
+<script context="module" lang="ts">
+  export const prerender = true
+</script>
+
+<script lang="ts">
+  let curators = []
+
+  getTeam()
+
+  async function getTeam() {
+    curators = await Promise.all(
+      Object.entries(import.meta.glob('/src/collections/curation-team/*.md')).map(
+        async ([path, module]) => {
+          const { metadata } = await module()
+          return { ...metadata }
+        },
+      ),
+    )
+  }
+</script>
+
+<svelte:head>
+  <title>Curation Team – BeastSaber</title>
+</svelte:head>
+
+<section>
+  {#each curators as curator}
+    {curator.name}
+  {/each}
+</section>
+
+<style>
+  section {
+    max-width: 1332px;
+  }
+</style>
