c580bad32133a60d2da1cde7495d008a7822e208
rorekk <rorekk@gmail.com>
2022-03-01 01:01:57 -0800

    Revert "fix for production cms"
    This reverts commit 2050da2f984fa0467006c0d3d935f05df1f7ac75.

--- prebuild.js
+++
@@ -1,5 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs'
-
-let config = readFileSync('./static/admin/config.yml', 'utf8')
-config = config.replace('local_backend: true', 'local_backend: false')
-writeFileSync('./static/admin/config.yml', config, 'utf8')
