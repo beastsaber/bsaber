307027c029f8532b10116d137450ce0bb9e5c595
rorekk <rorekk@gmail.com>
2022-03-06 01:45:13 -0800

    revert broken resizing

--- src/lib/resizer.ts
+++
@@ -1,8 +0,0 @@
-import sharp from 'sharp'
-
-export async function resize(sourcePath, width, height) {
-  sourcePath = sourcePath.replace('/static', 'static')
-  const targetPath = sourcePath.replace('static/uploads/', 'static/images/')
-  await sharp(sourcePath).resize(width, height).toFile(targetPath)
-  return targetPath.replace('static', '')
-}
