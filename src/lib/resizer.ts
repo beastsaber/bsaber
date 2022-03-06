ba85d5d9bf5d38d5a730a71270ad3096c9ce08c2
rorekk <rorekk@gmail.com>
2022-03-05 23:33:23 -0800

    fix build

--- src/lib/resizer.ts
+++ src/lib/resizer.ts
@@ -4,5 +4,5 @@ export async function resize(sourcePath, width, height) {
   sourcePath = sourcePath.replace('/static', 'static')
   const targetPath = sourcePath.replace('static/uploads/', 'static/images/')
   await sharp(sourcePath).resize(width, height).toFile(targetPath)
-  return targetPath
+  return targetPath.replace('static', '')
 }
