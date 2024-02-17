ea328c9e3365e25ef9b426e5825e03449fa1864a
Timothy Gillespie <timothy@gillespie.eu>
2024-02-17 20:32:38 +0100

    Use abstracted PostPage for Getting-Started

---
+++ src/collections/posts/specialty-getting-started.md
@@ -0,0 +1,45 @@
+---
+title: Getting Started with Beat Saber
+section: none
+publish: 2024-02-17T00:00:00.000Z
+category: Specialty Page
+image: /static/uploads/posts/beat-saber-1024x576.jpg
+---
+
+So you are new to Beat Saber? Maybe you have seen someone play a cool map on YouTube or TikTok but now that you bought the game you cannot find the map? Or maybe you just really like this game and would like to be part of the community? This guide is for you! We will explain to you the Beat Saber world and point you in the right direction to get started.
+
+# Finding and Playing Custom Maps
+
+One of the first things most players seek is how to get the maps you have seen somewhere else. The instructions will depend on your platform.
+
+You do not need mods to play custom maps, but they certainly are the easiest way to download them (see the modding section).
+
+If you prefer not installing any mods you can also download them yourself and put them in the appropriate game folder. See more details on the process [here](/getting-started/custom-songs/).
+
+Regardless of your method, you will most likely be getting them from [BeatSaver](https://beatsaver.com). You can search and view maps on there as well.
+
+# Modding
+
+Mods will allow you to download custom maps easily, compete in leaderboards, modify the game appearance and many more. Some maps may require additional mods to be playable. Refer to the [BSMG (Beat Saber Modding Group) Wiki](https://bsmg.wiki/beginners-guide.html) for instructions on how to mod the game.
+
+Do note, that some maps can also require mods to play. These mods can be either **Chroma**, **Noodle Extensions** and **Mapping Extensions**. If you particularly want to seek out these kind of maps you can find filters for them under the "Requirements" section as well!
+
+# Finding Good Maps
+
+Once you have broken out of beginner phase and regularly find yourself playing Expert+ maps you will find that the maps have quite a depth to them and distinct style to them.
+
+As BeastSaber, we want help you find great maps and such is the task of our curators.
+
+Maps on BeatSaver can marked as curated which means one of the curators considered them fun. You can [filter maps on BeatSaver](https://beatsaver.com/?curated=true) to only show curated maps.
+
+Additionally, you can checkout the [maps of the week](/maps-of-the-week/1) which are recent maps the curator team has considered particularly noteworthy.
+
+On our frontpage we also feature playlists, which are collections of maps that are all great and reviewed by the curators.
+
+# Competitive Play
+
+Sometimes, the fun is in competing. If that's you then you will want the respective mods for [BeatLeader](https://beatleader.xyz/) and [ScoreSaber](https://scoresaber.com/). Both of these provide leaderboards for you to compare with friends or the worlds on custom songs. Using the mods you'll be able to submit your own scores (with the replays), see the leaderboard in game and even collect performance points (short pp) which are used to rank you in the world.
+
+# Communities
+
+If you're here to mingle then you'll find many communities that might just be for you. We will be hosting an list of communities soon - so stay tuned!
--- src/routes/getting-started/+page.svelte
+++ src/routes/getting-started/+page.svelte
@@ -1,82 +1,11 @@
-<article>
-  <div class="title-header-container">
-    <h1 class="main-header">Getting Started with Beat Saber</h1>
-    <div class="darkening-container" />
-  </div>
+<script lang="ts">
+  import type { Post } from "../../types"
+  import PostPage from "$lib/PostPage.svelte"
+  import type { CollectionDataTypeMap } from "$lib/retrieveCollectionData"
 
-  <div class="article-body">
-    
-    <p>
-      So you are new to Beat Saber? Maybe you have seen someone play a cool map on YouTube or TikTok but now that you bought the game you cannot find the map? Or maybe you just really like this game and would like to be part of the community? This guide is for you! We will explain to you the Beat Saber world and point you in the right direction to get started.
-    </p>
-    
-    <h2>Finding and Playing Custom Maps</h2>
-    <p>One of the first things most players seek is how to get the maps you have seen somewhere else. The instructions will depend on your platform.</p>
-    <p>You do not need mods to play custom maps, but they certainly are the easiest way to download them (see the modding section).</p>
-    <p>If you prefer not installing any mods you can also download them yourself and put them in the appropriate game folder. See more details on the process <a href="/getting-started/custom-songs/">here</a>.</p>
-    <p>Regardless of your method, you will most likely be getting them from <a href="https://beatsaver.com">BeatSaver</a>. You can search and view maps on there as well.</p>
+  export let data: CollectionDataTypeMap["posts"];
+  if(data.body === undefined) throw new Error("No body found in post data");
+  const post: Post = {...data.attributes, body: data.body};
+</script>
 
-    <h2>Modding</h2>
-    <p>Mods will allow you to download custom maps easily, compete in leaderboards, modify the game appearance and many more. Some maps may require additional mods to be playable. Refer to the  <a href="https://bsmg.wiki/beginners-guide.html">BSMG (Beat Saber Modding Group) Wiki</a> for instructions on how to mod the game.</p>
-    <p>Do note, that some maps can also require mods to play. These mods can be either <b>Chroma</b>, <b>Noodle Extensions</b> and <b>Mapping Extensions</b>. If you particularly want to seek out these kind of maps you can find filters for them under the "Requirements" section as well!</p>
-
-    <h2>Finding Good Maps</h2>
-    <p>Once you have broken out of beginner phase and regularly find yourself playing Expert+ maps you will find that the maps have quite a depth to them and distinct style to them.</p>
-    <p>As BeastSaber, we want help you find great maps and such is the task of our curators.</p>
-    <p>Maps on BeatSaver can marked as curated which means one of the curators considered them fun. You can <a href="https://beatsaver.com/?curated=true">filter maps on BeatSaver</a> to only show curated maps.</p>
-    <p>Additionally, you can checkout the <a href="/maps-of-the-week/1">maps of the week</a> which are recent maps the curator team has considered particularly noteworthy.</p>
-    <p>On our frontpage we also feature playlists, which are collections of maps that are all great and reviewed by the curators.</p>
-
-
-    <h2>Competitive Play</h2>
-    <p>Sometimes, the fun is in competing. If that's you then you will want the respective mods for <a href="https://beatleader.xyz/">BeatLeader</a> and <a href="https://scoresaber.com/">ScoreSaber</a>. Both of these provide leaderboards for you to compare with friends or the worlds on custom songs. Using the mods you'll be able to submit your own scores (with the replays), see the leaderboard in game and even collect performance points (short pp) which are used to rank you in the world.</p>
-    
-    <h2>Communities</h2>
-    <p>If you're here to mingle then you'll find many communities that might just be for you. We will be hosting an list of communities soon - so stay tuned!</p>
-
-  </div>
-</article>
-
-<style lang="scss">
-  .main-header {
-    color: white;
-    font-size: 4rem;
-    text-shadow: 2px 2px 4px #000000;
-    z-index: 2;
-  }
-
-  $header-height: 30rem;
-  .title-header-container {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-position: center;
-    background-size: cover;
-    height: $header-height;
-    background-image: url('/Beat-Saber-1024x576.jpg');
-    z-index: 0;
-  }
-
-  .darkening-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.6);
-    z-index: 1;
-  }
-
-  .article-body {
-    margin: 3rem 5rem 3rem 5rem;
-  }
-
-  h2 {
-    margin-top: 2rem;
-  }
-
-  p {
-    margin-top: 0.5rem;
-  }
-</style>
+<PostPage post={post} />
