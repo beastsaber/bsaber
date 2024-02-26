5acaf45fe42b1ad0a12b563161810338dca197a1
Timothy Gillespie <timothy@gillespie.eu>
2024-02-26 18:20:09 +0100

    Fix pipeline by stubbing erroring file with note

--- resize-images.js
+++ resize-images.js
@@ -1,15 +1,1 @@
-import sharp from 'sharp'
-import fs from 'fs'
-
-resize('./static/uploads/curators', 80, 80)
-
-async function resize(directory, width, height) {
-  let filenames = fs.readdirSync(directory)
-
-  for (let filename of filenames) {
-    let path = `${directory}/${filename}`
-    let tempPath = `${directory}/temp-${filename}`
-    await sharp(path).resize(width, height).toFile(tempPath)
-    fs.renameSync(tempPath, path)
-  }
-}
+console.log('Please remove the command call "node resize-images.js" and then remove this script.')
