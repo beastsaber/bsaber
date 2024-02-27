7320043a83e2ebd1967da7b4a09f60307ff6674e
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2024-02-27 17:06:55 -0600

    Minor change to Beasties

--- src/routes/the-beastsaber-mapping-awards/+page.server.ts
+++ src/routes/the-beastsaber-mapping-awards/+page.server.ts
@@ -1,2 +1,2 @@
 import { retrieveCollectionData } from "$lib/retrieveCollectionData";
-export const load = () => retrieveCollectionData('posts', 'the-beastsaber-mapping-awards'); 
+export const load = () => retrieveCollectionData('posts', 'The-BeastSaber-Mapping-Awards'); 
