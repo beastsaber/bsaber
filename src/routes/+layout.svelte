b9b3155c4f91959c93f9be2f2248083ca46f7d28
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2024-03-09 14:54:20 -0600

    Actually fix margins/padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
     flex: 1;
     display: flex;
     flex-direction: column;
-    padding: 20px 12px;
+    padding: 20px 12px 2.3rem 12px;
     width: 100%;
     margin-top: 75px;
     box-sizing: border-box;
