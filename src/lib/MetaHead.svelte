36c9b13eda2a06005f22549bb691af79fec32dfc
Timothy Gillespie <timothy@gillespie.eu>
2024-03-10 02:03:26 +0100

    Fix some silly issues in the meta head and remove console.log

--- src/lib/MetaHead.svelte
+++ src/lib/MetaHead.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { page } from '$app/stores'
-  console.log($page.url.origin)
 
   export let title: string | undefined
   export let description: string | undefined
@@ -9,7 +8,11 @@
   let normalizedImage = imageUrl
   if (normalizedImage && !normalizedImage.startsWith('http') && normalizedImage.startsWith('/')) {
     normalizedImage = $page.url.origin + imageUrl
-  } else if (!normalizedImage?.startsWith('/') && !normalizedImage?.startsWith('http')) {
+  } else if (
+    normalizedImage &&
+    !normalizedImage.startsWith('/') &&
+    !normalizedImage.startsWith('http')
+  ) {
     if ($page.url.href.endsWith('/')) {
       normalizedImage = $page.url.href + imageUrl
     } else {
@@ -23,7 +26,7 @@
   <meta property="og:type" content="website" />
   <meta property="og:url" content={$page.url.href} />
 
-  #{#if title}
+  {#if title}
     <meta property="og:title" content={title} />
   {/if}
 
@@ -32,6 +35,6 @@
   {/if}
 
   {#if normalizedImage}
-    <meta property="og:image" content={normalizedImage} /> "
+    <meta property="og:image" content={normalizedImage} />
   {/if}
 </svelte:head>
