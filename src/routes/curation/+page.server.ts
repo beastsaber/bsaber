4f06076042eea65036e3772fa7df2fa8fbf66347
Timothy Gillespie <timothy@gillespie.eu>
2024-03-10 17:05:08 +0100

    Try using process.env

--- src/routes/curation/+page.server.ts
+++ src/routes/curation/+page.server.ts
@@ -1,6 +1,6 @@
 import { retrieveCollectionData } from '$lib/retrieveCollectionData'
 export const load = async () => {
   const output = await retrieveCollectionData('posts', 'curation')
-  output.test = import.meta.env.DEPLOY_PRIME_URL
+  output.test = process.env.DEPLOY_PRIME_URL
   return output
 }
