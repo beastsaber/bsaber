a421cebdc1876733767acc4f08b4b610a6b0eb57
Timothy Gillespie <timothy@gillespie.eu>
2024-03-16 23:21:28 +0100

    Sort by activity level first

--- src/routes/community-hub/+page.server.ts
+++ src/routes/community-hub/+page.server.ts
@@ -6,9 +6,23 @@ export type CommunityHubSSRData = {
   availableLabels: (CommunityLabel & { activated: boolean })[]
 }
 
+const activityLevelMapping = {
+  low: 1,
+  medium: 2,
+  high: 3,
+}
+
 export const load = async (): Promise<CommunityHubSSRData> => {
   const communityCollections = (await retrieveAllCollectionDataOfType('communities')).sort(
     (a, b) => {
+      const aActivityLevel = activityLevelMapping[a.attributes.activityLevel]
+      const bActivityLevel = activityLevelMapping[b.attributes.activityLevel]
+      if (aActivityLevel > bActivityLevel) {
+        return -1
+      } else if (aActivityLevel < bActivityLevel) {
+        return 1
+      }
+
       return a.attributes.name.localeCompare(b.attributes.name)
     },
   )
