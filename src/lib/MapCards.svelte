53b00ddf58d6042ca7dc73137f5754b150915a50
Timothy Gillespie <timothy@gillespie.eu>
2024-04-02 21:26:05 +0200

    Add OneClick Download button to map cards

--- src/lib/MapCards.svelte
+++ src/lib/MapCards.svelte
@@ -1,59 +1,74 @@
 <script lang="ts">
-    import type {Beatmap} from "../types"
-    import {onMount} from "svelte";
-    import Uploader from "$lib/Uploader.svelte";
-    import Tags from "$lib/Tags.svelte";
-    import Difficulties from "$lib/Difficulties.svelte";
+  import type { Beatmap } from '../types'
+  import { onMount } from 'svelte'
+  import Uploader from '$lib/Uploader.svelte'
+  import Tags from '$lib/Tags.svelte'
+  import Difficulties from '$lib/Difficulties.svelte'
+  import OneClickDownloadButton from './OneClickDownloadButton.svelte'
 
-    export let sortOrder: "FIRST_PUBLISHED" | "UPDATED" | "LAST_PUBLISHED" | "CREATED" | "CURATED" = "FIRST_PUBLISHED"
-    export let verified: boolean | undefined = undefined
-    export let maxCards: number | undefined = undefined // max amount of cards to show
+  export let sortOrder: 'FIRST_PUBLISHED' | 'UPDATED' | 'LAST_PUBLISHED' | 'CREATED' | 'CURATED' =
+    'FIRST_PUBLISHED'
+  export let verified: boolean | undefined = undefined
+  export let maxCards: number | undefined = undefined // max amount of cards to show
 
-    let maps: Beatmap[] = []
+  let maps: Beatmap[] = []
 
-    onMount(async () => {
-        await getMaps()
-    })
+  onMount(async () => {
+    await getMaps()
+  })
 
-    let baseUrl = import.meta.env.VITE_BEATSAVER_API_BASE || 'https://api.beatsaver.com'
-    let url = `${baseUrl}/maps/latest?sort=${sortOrder}${(verified !== undefined) ? `&verified=${verified}` : ""}&pageSize=${maxCards ?? 8}`
+  let baseUrl = import.meta.env.VITE_BEATSAVER_API_BASE || 'https://api.beatsaver.com'
+  let url = `${baseUrl}/maps/latest?sort=${sortOrder}${
+    verified !== undefined ? `&verified=${verified}` : ''
+  }&pageSize=${maxCards ?? 8}`
 
-    async function getMaps() {
-        let response = await fetch(url)
-        maps = await response.json().then(json =>
-            json["docs"] as Beatmap[]
-        )
-    }
+  async function getMaps() {
+    let response = await fetch(url)
+    maps = await response.json().then((json) => json['docs'] as Beatmap[])
+  }
 </script>
 
 <div class="cards max-cols-3">
-    {#if maps.length !== 0}
-        {#each maps as map}
-            <div class="card">
-                <a href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/maps/${map.id}`} class="image-link">
-                    <img src="{`${import.meta.env.VITE_BEATSAVER_CDN_BASE || 'https://cdn.beatsaver.com'}/${map.versions[0].hash}.jpg`}" alt="{map.name}"/>
-                </a>
-                <div class="content">
-                    <div>
-                        <a href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/maps/${map.id}`}
-                           class="title"
-                           title="{map.name}">
-                            {map.name}
-                        </a>
-                        <Uploader uploader="{map.uploader}" curator="{map.curator}"/>
-                    </div>
-                    <Tags tags="{map.tags}"/>
-                    <Difficulties diffs="{map.versions[0].diffs}"/>
-                </div>
-            </div>
-        {/each}
-    {:else}
-        {#each Array(maxCards ?? 8) as _}
-            <div class="card loading"/>
-        {/each}
-    {/if}
+  {#if maps.length !== 0}
+    {#each maps as map}
+      <div class="card">
+        <a
+          href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/maps/${map.id}`}
+          class="image-link"
+        >
+          <img
+            src={`${import.meta.env.VITE_BEATSAVER_CDN_BASE || 'https://cdn.beatsaver.com'}/${
+              map.versions[0].hash
+            }.jpg`}
+            alt={map.name}
+          />
+        </a>
+        <div class="content">
+          <div>
+            <a
+              href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/maps/${
+                map.id
+              }`}
+              class="title"
+              title={map.name}
+            >
+              {map.name}
+            </a>
+            <Uploader uploader={map.uploader} curator={map.curator} />
+          </div>
+          <Tags tags={map.tags} />
+          <div class="last-row-container">
+            <Difficulties diffs={map.versions[0].diffs} /><OneClickDownloadButton mapId={map.id} />
+          </div>
+        </div>
+      </div>
+    {/each}
+  {:else}
+    {#each Array(maxCards ?? 8) as _}
+      <div class="card loading" />
+    {/each}
+  {/if}
 </div>
-
 
 <style lang="scss">
   @import 'src/scss/variables';
@@ -63,7 +78,8 @@
   .cards {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
-    gap: 1.25rem;
+    gap: 1.25rem 4rem;
+    width: 100%;
 
     @media (min-width: 992px) {
       grid-template-columns: repeat(2, 1fr);
@@ -71,6 +87,7 @@
   }
 
   .card {
+    width: 100%;
     position: relative;
     display: flex;
     overflow: hidden;
@@ -93,6 +110,7 @@
     }
 
     .content {
+      width: 100%;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
@@ -106,6 +124,13 @@
         overflow: hidden;
         text-overflow: ellipsis;
         margin-bottom: 0.25rem;
+      }
+
+      .last-row-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
       }
     }
   }
