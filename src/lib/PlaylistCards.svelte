8560ddcda07dce2b73ef1f2e6b0201a9ea4dcf1b
Timothy Gillespie <timothy@gillespie.eu>
2024-04-02 21:32:05 +0200

    Add OneClick to playlist cards

--- src/lib/PlaylistCards.svelte
+++ src/lib/PlaylistCards.svelte
@@ -1,45 +1,55 @@
 <script lang="ts">
-    import type {Playlist} from "../types"
-    import {onMount} from "svelte";
-    import Uploader from '$lib/Uploader.svelte'
+  import type { Playlist } from '../types'
+  import { onMount } from 'svelte'
+  import Uploader from '$lib/Uploader.svelte'
+  import OneClickDownloadButton from './OneClickDownloadButton.svelte'
 
-    export let maxCards: number | undefined = undefined // max amount of cards to show
+  export let maxCards: number | undefined = undefined // max amount of cards to show
 
-    let playlists: Playlist[] = []
+  let playlists: Playlist[] = []
 
-    onMount(async () => {
-        await getPlaylists()
-    })
+  onMount(async () => {
+    await getPlaylists()
+  })
 
-    async function getPlaylists() {
-        let response = await fetch(
-            `${import.meta.env.VITE_BEATSAVER_API_BASE || 'https://api.beatsaver.com'}/playlists/latest?sort=CURATED&pageSize=${maxCards ?? 4}`,
-        )
-        playlists = await response.json().then(json =>
-            json["docs"] as Playlist[]
-        )
-    }
+  async function getPlaylists() {
+    let response = await fetch(
+      `${
+        import.meta.env.VITE_BEATSAVER_API_BASE || 'https://api.beatsaver.com'
+      }/playlists/latest?sort=CURATED&pageSize=${maxCards ?? 4}`,
+    )
+    playlists = await response.json().then((json) => json['docs'] as Playlist[])
+  }
 </script>
 
 <div class="cards max-cols-4" style="--aspect-ratio: 1">
-    {#if playlists.length !== 0}
-        {#each playlists as playlist}
-            <a class="card"
-               href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/playlists/${playlist.playlistId}`}
-               style={`background-image: url(${playlist.playlistImage512})`}>
-                <div class="content max-cols-4">
-                    <Uploader uploader="{playlist.owner}"/>
-                    <h3 class="title">{playlist.name ?? ''}</h3>
-                </div>
-            </a>
-        {/each}
-    {:else}
-        {#each Array(maxCards ?? 4) as _}
-            <div class="card loading"/>
-        {/each}
-    {/if}
+  {#if playlists.length !== 0}
+    {#each playlists as playlist}
+      <a
+        class="card"
+        href={`${import.meta.env.VITE_BEATSAVER_BASE || 'https://beatsaver.com'}/playlists/${
+          playlist.playlistId
+        }`}
+        style={`background-image: url(${playlist.playlistImage512})`}
+      >
+        <div class="content max-cols-4">
+          <Uploader uploader={playlist.owner} />
+          <div class="last-row-container">
+            <h3 class="title">{playlist.name ?? ''}</h3>
+            <OneClickDownloadButton
+              playlistUrl="https://api.beatsaver.com/playlists/id/{playlist.playlistId}/download"
+            />
+          </div>
+        </div>
+      </a>
+    {/each}
+  {:else}
+    {#each Array(maxCards ?? 4) as _}
+      <div class="card loading" />
+    {/each}
+  {/if}
 </div>
 
 <style lang="scss">
-  @import "../scss/post-cards";
+  @import '../scss/post-cards';
 </style>
