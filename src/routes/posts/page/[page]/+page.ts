32a56f36f388f1e0b86bc416f9dfb34c7e20f58e
Timothy Gillespie <timothy@gillespie.eu>
2024-04-03 15:22:34 +0200

    Paginate posts page

---
+++ src/routes/posts/page/[page]/+page.ts
@@ -0,0 +1,53 @@
+import type { Post } from '../../../../types'
+
+const pageSize = 10
+
+type LoadParameter = {
+  params: {
+    page: string
+  }
+  fetch: typeof fetch
+}
+
+export type PostPagePaginatedSSRData = {
+  posts: Post[]
+  pageSize: number
+  pageCount: number
+  currentPage: number
+}
+
+export async function load({ params: { page } }: LoadParameter): Promise<PostPagePaginatedSSRData> {
+  const pageNumber = parseInt(page, 10)
+
+  const allPostData: Post[] = (
+    await Promise.all(
+      Object.entries(import.meta.glob('/src/collections/posts/*.md')).map(
+        async ([path, module]) => {
+          const { metadata } = await module()
+          const slug = path.split('/').reverse()[0].split('.')[0]
+          const { image } = metadata
+          return {
+            slug,
+            ...metadata,
+            image: image?.replace('/static', ''),
+            publishDate: Date.parse(metadata.publish),
+          }
+        },
+      ),
+    )
+  )
+    .filter((x) => x.showInPostListing)
+    .sort((a, b) => b.publishDate - a.publishDate)
+
+  const startIndex = (pageNumber - 1) * pageSize
+  const endIndex = pageNumber * pageSize
+  const paginatedPosts = allPostData.slice(startIndex, endIndex)
+  const pageCount = Math.ceil(allPostData.length / pageSize)
+
+  return {
+    posts: paginatedPosts,
+    pageSize,
+    pageCount,
+    currentPage: pageNumber,
+  }
+}
