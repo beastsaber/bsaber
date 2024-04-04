141f0af07863cc6549763616456794fa0b677c0e
Pixelguymm <maartenmarx@hotmail.be>
2024-04-05 00:56:23 +0200

    Community Hub style overhaul

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -207,7 +207,7 @@
   .dropdown-toggle::after {
     display: inline-block;
     margin-left: 0.255em;
-    vertical-align: 0.255em;
+    vertical-align: 0.15em;
     content: '';
     border-top: 0.3em solid;
     border-right: 0.3em solid transparent;
