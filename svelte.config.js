1ae4957d39976bf17ba47c2cc6e0d8ed92a2a5c7
Timothy Gillespie <timothy@gillespie.eu>
2024-04-21 22:07:31 +0200

    Remove unused config comments

--- svelte.config.js
+++ svelte.config.js
@@ -29,22 +29,6 @@ const config = {
     prerender: {
       crawl: true,
     },
-
-    // Content-Security-Policy Configuration
-    // csp: {
-    //   mode: 'auto',
-    //   directives: {
-    //     'default-src': ['self', 'https://api.bsaber.com/'],
-    //     'script-src': [''],
-    //     'style-src': ['self'],
-    //     'img-src': ['self', 'https://cdn.scoresaber.com/avatars/', 'data:'],
-    //     'upgrade-insecure-requests': true,
-    //     'frame-ancestors': ['self', 'https://questmodding.com'],
-    //     'object-src': ['none'],
-    //     'base-uri': ['self'],
-    //     'connect-src': ['self', 'https://api.bsaber.com/', 'https://api.beatsaver.info'],
-    //   },
-    // },
   },
   extensions: ['.svelte', '.svelte.md', '.md', '.svx'],
 }
