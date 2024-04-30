be6080d77953d2f848378bbb7c9708b8a94ce2e9
Timothy Gillespie <timothy@gillespie.eu>
2024-04-30 21:20:59 +0200

    Fix error page setup

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,8 @@
 <script>
+  import MetaHead from '$lib/MetaHead.svelte'
   import ErrorPage from './__error.svelte'
 </script>
+
+<MetaHead title="Not Found" />
 
 <ErrorPage title="Not Found" errorCode="404" errorMessage="Not Found" />
