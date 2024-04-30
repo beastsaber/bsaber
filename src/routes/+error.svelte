62d805ff522052f7c0348a0474eb93e51597e814
Timothy Gillespie <timothy@gillespie.eu>
2024-04-30 21:22:15 +0200

    Add error specific description

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,6 @@
   import ErrorPage from './__error.svelte'
 </script>
 
-<MetaHead title="Not Found" />
+<MetaHead title="Not Found" description="This page has not been found." />
 
 <ErrorPage title="Not Found" errorCode="404" errorMessage="Not Found" />
