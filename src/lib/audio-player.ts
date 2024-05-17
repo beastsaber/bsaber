844f8dfc46caac8b69eb5688c30c0d48d2c1830d
Timothy Gillespie <timothy@gillespie.eu>
2024-05-17 21:19:49 +0200

    Add playing id to audio player to help displaying a stop button

--- src/lib/audio-player.ts
+++ src/lib/audio-player.ts
@@ -1,13 +1,16 @@
 class AudioPlayer {
   audioElement: HTMLAudioElement = document.createElement('audio')
+  playingId: string | null = null
 
-  play(url: string) {
+  play(url: string, id?: string) {
     this.audioElement.src = url
     this.audioElement.play()
+    this.playingId = id || null
   }
 
   pause() {
     this.audioElement.pause()
+    this.playingId = null
   }
 }
 
