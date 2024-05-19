a399a58ac359101df4ea9c1718c5c024a918e100
Timothy Gillespie <timothy@gillespie.eu>
2024-05-19 20:39:57 +0200

    Extract icons from community hub page

---
+++ src/lib/iconMapping.ts
@@ -0,0 +1,24 @@
+import {
+  faDiscord,
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faReddit,
+  faTwitch,
+  faXTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+import { faBook, faLink } from '@fortawesome/free-solid-svg-icons'
+
+export const iconMapping = {
+  Discord: faDiscord,
+  'Twitter/X': faXTwitter,
+  Facebook: faFacebook,
+  Instagram: faInstagram,
+  Twitch: faTwitch,
+  YouTube: faYoutube,
+  Reddit: faReddit,
+  GitHub: faGithub,
+  Website: faLink,
+  Documentation: faBook,
+}
