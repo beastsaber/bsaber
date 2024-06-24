7771669ddfba1c8cb323c7aeb4fdb7ea0d82dc7b
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2024-06-23 20:09:24 -0500

    Updated effect

--- src/routes/community-hub/+page.svelte
+++ src/routes/community-hub/+page.svelte
@@ -485,38 +485,16 @@
     }
 
     &.activity-high {
-      .community-image img {
-        position: relative;
-        animation: rotateShadow 4s infinite linear;
+      animation: colorTransition 3s linear infinite alternate;
+    }
+
+    @keyframes colorTransition {
+      0%,
+      100% {
+        box-shadow: 0 0 15px 5px $color-bsaber-purple;
       }
-      @keyframes rotateShadow {
-        0% {
-          box-shadow: 10px 0 20px #7646af;
-        }
-        12.5% {
-          box-shadow: 7.07px 7.07px 20px #7646af;
-        }
-        25% {
-          box-shadow: 0 10px 20px #7646af;
-        }
-        37.5% {
-          box-shadow: -7.07px 7.07px 20px #7646af;
-        }
-        50% {
-          box-shadow: -10px 0 20px #7646af;
-        }
-        62.5% {
-          box-shadow: -7.07px -7.07px 20px #7646af;
-        }
-        75% {
-          box-shadow: 0 -10px 20px #7646af;
-        }
-        87.5% {
-          box-shadow: 7.07px -7.07px 20px #7646af;
-        }
-        100% {
-          box-shadow: 10px 0 20px #7646af;
-        }
+      50% {
+        box-shadow: 0 0 15px 5px $color-difficulty-purple;
       }
     }
   }
