af1ffd7dd15f562be8a288692961dc74f46dd7b2
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2024-07-07 19:38:33 -0500

    Initial recreation of footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,44 @@
+<footer>
+  <hr />
+  <div class="footer-content">
+    <div class="logo">
+      <a href="https://bsaber.com"> <img alt="BeastSaber" src="/BeastSaber-LogoW.webp" /></a>
+    </div>
+    <div class="links">
+      <p>
+        <a class="btn" href="https://discord.gg/VJZHUbt" target="_blank">Discord</a> <br />
+        <a class="btn" href="https://www.patreon.com/user/membership?u=119426555" target="_blank"
+          >Support Us</a
+        > <br />
+        <a class="btn" href="https://www.bsaber.com" target="_blank">Privacy Policy</a> <br />
+        <a class="btn" href="https://www.bsaber.com" target="_blank">Chaeyoung</a> <br />
+      </p>
+    </div>
+  </div>
+</footer>
+
+<style lang="scss">
+  @import 'src/scss/variables';
+  .footer-content {
+    position: relative;
+    text-align: center;
+    padding: 1.5rem 0 1.5rem 0;
+  }
+
+  hr {
+    width: 40%;
+    margin: 0 auto;
+    filter: brightness(45%);
+  }
+
+  .btn {
+    color: white;
+    font-family: $font-poppins;
+    font-size: 1.1rem;
+    line-height: 1.5;
+  }
+
+  .logo {
+    scale: 54%;
+  }
+</style>
