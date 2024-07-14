a3971abbb50c29738fa52043d6732f77d9b6ea5e
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2024-07-13 23:00:37 -0500

    Use infinitely better styling for featured playlists

--- src/routes/playlists/fox-stevenson-pack/+page.svelte
+++ src/routes/playlists/fox-stevenson-pack/+page.svelte
@@ -11,23 +11,32 @@
 
 <section class="hero">
   <div class="content">
-    <p>
-      <img src="/uploads/playlists/alphabeat.jpg" alt="Alphabeat Logo" width="100" height="100" />
-    </p>
-    Alphabeat Presents
-    <h1>Fox Stevenson Pack</h1>
-    <p>
-      Alphabeat presents their newest pack, featuring songs by Fox Stevenson (formerly Stan SB /
-      Stan SSB)!
-      <br />
-      He is an English singer-songwriter, DJ and producer of bass music, mostly drum and bass and glitch
-      hop.
-      <br />
-      We hope you enjoy!
-    </p>
+    <div class="card-container">
+      <div class="card">
+        <a href="https://beatsaver.com/profile/5122" target="_blank"
+          ><img src="/uploads/playlists/alphabeat1.jpg" alt="Alphabeat" />
+        </a>
+      </div>
+    </div>
+    <div class="uploader-container">
+      <a class="uploader" href="https://beatsaver.com/profile/5122" target="_blank">Alphabeat</a>
+      Presents
+    </div>
+    <div class="body">
+      <h1>Fox Stevenson Pack</h1>
+      <p>
+        Alphabeat presents their newest pack, featuring songs by Fox Stevenson (formerly Stan SB /
+        Stan SSB)!
+        <br />
+        He is an English singer-songwriter, DJ and producer of bass music, mostly drum and bass and glitch
+        hop.
+        <br />
+        We hope you enjoy!
+      </p>
+    </div>
     <div class="buttons">
-      <a class="btn" href="https://api.beatsaver.com/playlists/id/89418/download" target="_blank"
-        >DOWNLOAD PLAYLIST</a
+      <a class="btn" href="https://api.beatsaver.com/playlists/id/581099/download" target="_blank"
+        >ZIP DOWNLOAD</a
       >
       <a class="btn2" href="https://beatsaver.com/playlists/581099" target="_blank">BEATSAVER</a>
     </div>
@@ -44,19 +53,12 @@
     min-height: 45rem;
     height: 70vh;
     background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    margin-top: -28px;
+    margin-top: -20px;
     overflow: hidden;
   }
-
   .hero::before {
     content: '';
     position: absolute;
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
     background: url('/uploads/playlists/fox-red.png') no-repeat center center;
@@ -65,14 +67,45 @@
     z-index: -1;
   }
 
-  img {
-    padding-bottom: 10px;
+  .content {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
   }
 
-  .content {
-    position: relative;
-    z-index: 1;
-    bottom: 6rem;
+  .card-container {
+    display: flex;
+    gap: 2.5rem;
+    height: 200px;
+    transform: scale(15%);
+    margin-top: -5rem;
+  }
+  .card {
+    height: 700px;
+    background: rgba(48, 208, 232, 0);
+    flex: 1;
+    width: 45rem;
+  }
+  .card img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 25px;
+  }
+
+  .uploader {
+    color: white;
+    font-weight: 1000;
+  }
+
+  .body {
+    padding-bottom: 2.5rem;
   }
 
   h1 {
@@ -80,7 +113,7 @@
     width: 100%;
     justify-content: center;
     text-align: center;
-    margin-top: 2rem;
+    margin-top: 1rem;
   }
   @media (max-width: 678px) {
     h1 {
@@ -96,19 +129,23 @@
   p {
     margin: 0.5rem 0.5rem 0;
   }
+
+  .buttons {
+    display: grid;
+    gap: 0.5rem;
+    justify-content: center;
+    width: 100%;
+  }
   .btn,
   .btn2 {
-    border: none;
     padding: 12px 30px;
     cursor: pointer;
     font-size: 15px;
-    position: absolute;
     text-align: center;
-    width: 165px;
-    justify-content: center;
     transition: background-color 0.5s ease;
     border-radius: 5px;
     font-weight: bolder;
+    width: 165px;
   }
 
   .btn:hover {
@@ -125,17 +162,11 @@
   .btn {
     background-color: #fffdfd;
     color: black;
-    top: 125%;
-    left: 50%;
-    transform: translate(-50%, -50%);
   }
 
   .btn2 {
     background-color: #fffdfd;
     color: black;
-    top: 140%;
-    left: 50%;
-    transform: translate(-50%, -50%);
   }
 
   hr.fade {
