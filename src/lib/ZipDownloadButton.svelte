4e6eb1e1df8f0f617bdfb7a16c7911de3e5518fb
EnderdracheLP <enderdrachelp@elp.quest>
2025-04-11 20:32:03 +0200

    Update ZipDownloadButton component to svelte 5

--- src/lib/ZipDownloadButton.svelte
+++ src/lib/ZipDownloadButton.svelte
@@ -2,8 +2,12 @@
   import Fa from 'svelte-fa'
   import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
-  export let downloadURL: string
-  export let fontSize: string = '1rem'
+  interface Props {
+    downloadURL: string;
+    fontSize?: string;
+  }
+
+  const { downloadURL, fontSize = '1rem' }: Props = $props();
 </script>
 
 <a
