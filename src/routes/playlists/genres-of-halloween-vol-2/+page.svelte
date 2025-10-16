9666c27de0d492b5763170d96f98242a94e382e3
TheCzar1994 <104277242+TheCzar1994@users.noreply.github.com>
2025-10-15 21:08:21 -0500

    Actually update the embed image

--- src/routes/playlists/genres-of-halloween-vol-2/+page.svelte
+++ src/routes/playlists/genres-of-halloween-vol-2/+page.svelte
@@ -10,7 +10,7 @@
 <MetaHead
   title="Genres of Halloween - Vol. 2"
   description="Ready for Round 2?! TG Mapping Group returns, for Vol. 2 of Genres of Halloween!"
-  imageUrl="/uploads/playlists/swarm-pack/swarm-gone.jpg"
+  imageUrl="/uploads/playlists/1033478.jpg"
 />
 
 <section class="hero">
